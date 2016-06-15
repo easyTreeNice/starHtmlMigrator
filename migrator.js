adb8665bd0c4252415df9aa68fd0fa907b5c6d9a
Lee Doran-Constant <lee@doran-constant.co.uk>
2016-06-15 15:21:43 +0100

    added demo mode

--- migrator.js
+++ migrator.js
@@ -117,8 +117,22 @@ function display(data) {
     });
 }
 
+var qs = (function (a) {
+    if (a == "") return {};
+    var b = {};
+    for (var i = 0; i < a.length; ++i) {
+        var p = a[i].split('=', 2);
+        if (p.length == 1)
+            b[p[0]] = "";
+        else
+            b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+    }
+    return b;
+})(window.location.search.substr(1).split('&'));
+
 $(function () {
-    visualTidy();
+    var demo = parseInt(qs['demo'], 10);
+    if (demo >= 1) visualTidy();
     var data = extract();
-    display(data);
+    if (demo >= 2) display(data);
 });
